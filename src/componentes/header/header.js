cec90b41ddacce6129b2659220f3334f8e4856a7
romantkacik <123880795+romantkacik@users.noreply.github.com>
2024-01-10 11:29:17 +0100

    final sin button buscar+ funcionando

--- src/componentes/header/header.js
+++ src/componentes/header/header.js
@@ -73,28 +73,5 @@ export const headerContainer = (accesKey, sectionCards) => {
   divIndex.appendChild(userLink)
   divContainerNav.appendChild(divIndex)
 
-  formSearch.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const keyword = inputSearch.value.trim()
-    if (keyword !== '') {
-      // Llama a la función pasando las variables necesarias
-      getPhotosByTerm(accesKey, sectionCards, keyword)
-    }
-  })
-  buscarMas.addEventListener('click', (event) => {
-    event.preventDefault()
-    const keyword = inputSearch.value.trim()
-
-    if (keyword !== '') {
-      // Si hay un término de búsqueda, carga fotos con ese término
-      getPhotosByTerm(accesKey, sectionCards, keyword)
-    } else {
-      // Si no hay un término de búsqueda, carga fotos aleatorias
-      getRandomPhotos(accesKey, sectionCards)
-    }
-    // Hacer scroll al final de la página
-    window.scrollTo(0, document.body.scrollHeight)
-  })
-
   return divContainerNav
 }
