e61bff225292b3da78ac312e616180980ede89af
romantkacik <123880795+romantkacik@users.noreply.github.com>
2024-01-12 16:22:48 +0100

    arreglos Daniel

--- src/componentes/cards/cards.js
+++ src/componentes/cards/cards.js
@@ -1,4 +1,5 @@
 // cards.js
+
 import './cards.css'
 
 export const getRandomPhotos = (accesKey, sectionCards) => {
@@ -17,7 +18,7 @@ export const getRandomPhotos = (accesKey, sectionCards) => {
     .catch((error) => console.error('Error fetching random photos:', error))
 }
 
-export const getPhotosByTerm = (accesKey, sectionCards, keyword, page) => {
+export const getPhotosByTerm = (accesKey, sectionCards, keyword, page = 1) => {
   const searchUrl = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&per_page=9&client_id=${accesKey}`
 
   fetch(searchUrl)
@@ -26,12 +27,31 @@ export const getPhotosByTerm = (accesKey, sectionCards, keyword, page) => {
       sectionCards.innerHTML = ''
       console.log(res.results)
 
-      for (const photo of res.results) {
-        const img = document.createElement('img')
-        img.className = 'Card'
-        img.src = photo.urls.small
-        sectionCards.appendChild(img)
+      if (res.results.length === 0) {
+        // Si no hay resultados, mostrar un mensaje al usuario
+        displayNoResultsMessage(sectionCards)
+      } else {
+        // Si hay resultados, mostrar las fotos
+        for (const photo of res.results) {
+          const img = document.createElement('img')
+          img.className = 'Card'
+          img.src = photo.urls.small
+          sectionCards.appendChild(img)
+        }
       }
     })
     .catch((error) => console.error('Error fetching search results:', error))
+}
+
+const displayNoResultsMessage = (sectionCards) => {
+  // Elimina cualquier contenido existente en la sección de tarjetas
+  sectionCards.innerHTML = ''
+
+  // Crea un elemento de mensaje
+  const noResultsMessage = document.createElement('p')
+  noResultsMessage.textContent = 'No se han obtenido resultados !!!'
+  noResultsMessage.className = 'noResults'
+
+  // Agrega el mensaje a la sección de tarjetas
+  sectionCards.appendChild(noResultsMessage)
 }
